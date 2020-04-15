2ccdb6e8add6ae4ab16ed45d5ec14fd722c2aed7
vinicfrancisco <vini.ibi@gmail.com>
2020-04-15 09:47:36 -0300

    created users routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/apointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
