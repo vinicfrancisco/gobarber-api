42dd5fc12ce6b36ba29ac525ff07286963ebb670
vinicfrancisco <vini.ibi@gmail.com>
2020-05-04 15:41:20 -0300

    fixed databse connection error

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -7,7 +7,6 @@ import CreateAppointmentsService from '@modules/appointments/services/CreateAppo
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -22,6 +21,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
   const parsedDate = parseISO(date);
 
+  const appointmentsRepository = new AppointmentsRepository();
   const createAppointment = new CreateAppointmentsService(
     appointmentsRepository,
   );
